c92d7ef92b4df7df8730b52b527d00ad014c72af
Vitali Homenko <vitali.homenko@gmail.com>
2015-01-30 22:31:05 +0100

    disable autosave

--- dev_settings.php
+++ dev_settings.php
@@ -1,5 +1,11 @@
 <?php
 
+
+function wp_autosave_dequeue_script() {
+   wp_dequeue_script( 'autosave' );
+}
+add_action( 'admin_print_scripts', 'wp_autosave_dequeue_script', 100 );
+
 const ADI_NEWS_CAT_ID = 2;
 const ADI_EVENTS_CAT_ID = 5;
 const ADI_EVENTS_ARCHIVE_CAT_ID = 6;
